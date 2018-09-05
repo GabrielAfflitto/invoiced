d57e8556067c953e0d25fb25229e096e181e80db
GabrielAfflitto <gafflitto92@gmail.com>
2018-09-05 15:35:40 -0600

    added CORS gem and made configurations to be able to communicate with separate frontend

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,11 @@ module Invoiced
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:patch, :put, :delete, :get, :post, :options]
+      end
+    end
   end
 end
